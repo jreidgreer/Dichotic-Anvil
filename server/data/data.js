5cd7ab4d819cae1f2820caf7b45b5da9bcbec830
Hack Reactor Students <accounts@hackreactor.com>
2016-04-08 20:25:32 -0700

    mock data

---
+++ server/data/data.js
@@ -0,0 +1,116 @@
+var userData = [
+  {
+    username: 'Michael',
+    inventory: [
+    	{
+      		itemName: 'Charlottes Web',
+      		borrowed: false,
+      		url: 'https://bi.hcpdts.com/page/450/EwIaWqDxBJPJUu7rJh2VzSLlPeM!iR6JEYqIC4vJLZnl+pDL14wdGFL3HP9J!2Y4B0IX9EVzs!jwPJDv662h7pSRgjDoYa4D0WEjXV2Xa+y4I8Sa35lqltGQDsxSA7ym/UrP4fwuq1G3L+lCQHXVjJ4WD9n1O4!fHVzU32t1zotb2XltGqt5NH08Zg1lv!rMx0rUDeeqoUwC9Vrx87vEQ1LSjaF5npgVSlS9ewe+XLyc='
+    	},
+    	{
+      		itemName: 'Drill',
+      		borrowed: false,
+      		url: 'http://s3-production.bobvila.com/blogs/wp-content/uploads/2013/03/power-drill.jpg'
+    	},
+    	{
+      		itemName: 'Baby Crib',
+      		borrowed: false,
+      		url: 'http://theeastcoastorganicmattressstore.com/wp-content/uploads/2013/10/HUB_0372.png'
+    	}
+    	
+    ],
+    password: '1q2w3e4r'
+  },
+  
+  {
+    username: 'John',
+    inventory: [
+    	{
+      		itemName: 'Xbox',
+      		borrowed: false,
+      		url: 'https://compass-ssl.xbox.com/assets/23/0d/230dc52a-8f0e-40bf-bbd1-c51fdb8371e3.png?n=Homepage-360-UA_Upgrade-big_1056x594.png'
+    	},
+    	{
+      		itemName: 'saw',
+      		borrowed: true,
+      		url: 'http://www.winwintools.com/upfiles/Products/2011042226145575.jpg'
+    	},
+    	{
+      		itemName: 'Disco Ball',
+      		borrowed: false,
+      		url: 'http://nyshowplace.com/additionalPages/error404.aspx?aspxerrorpath=/content/images/800x600/DiscoBall.JPG'
+    	}
+    	
+    ],
+    password: 'q1w2e3r4'
+  	},
+  	
+    {
+      username: 'tStewart',
+      inventory: [
+    	{
+      		itemName: 'Rice Cooker',
+      		borrowed: true,
+      		url: 'https://ll-us-i5.wal.co/dfw/dce07b8c-5b4a/k2-_f4a3ec6c-636e-4da8-be66-f708de7c419b.v1.jpg-c86c3d363cfb7c043a3180d28c03c75402eb0b6c-optim-450x450.jpg'
+    	},
+    	{
+      		itemName: 'Hammock',
+      		borrowed: false,
+      		url: 'http://america.pink/images/1/8/6/6/7/0/4/en/2-hammock.jpg'
+    	},
+    	{
+      		itemName: 'Trailer',
+      		borrowed: false,
+      		url: 'http://imageserv11.team-logic.com/mediaLibrary/202/lgDSCN0230_1.JPG'
+    	}
+    	
+    ],
+    password: 'sa4j56k87'
+  },
+  
+  {
+      username: 'jBurwell',
+      inventory: [
+    	{
+      		itemName: 'Table',
+      		borrowed: true,
+      		url: 'https://static1.squarespace.com/static/5241b934e4b0ab230e3a8ba1/52f2a2a4e4b06e715ce128d1/52f2a479e4b09c7f1bf3cc31/1391633597880/Fold+in+half+1.jpg'
+    	},
+    	{
+      		itemName: 'Punch Bowl',
+      		borrowed: false,
+      		url: 'http://images.cb2.com/is/image/CB2/PunchBowlWLadleF13/&$web_zoom$&/131014103054/punch-bowl-with-ladle.jpg'
+    	},
+    	{
+      		itemName: 'Tarpaulin',
+      		borrowed: false,
+      		url: 'http://www.vkplastlam.com/images/demo2/123.png'
+    	}
+    	
+    ],
+    password: 'qw76jg2hk6m'
+  },
+  
+  {
+      username: 'cMendez',
+      inventory: [
+    	{
+      		itemName: 'Underwater Camera',
+      		borrowed: true,
+      		url: 'http://ourcelebratedmoments.com/wp-content/uploads/2013/03/underwater-camera.jpg'
+    	},
+    	{
+      		itemName: 'Party Tent',
+      		borrowed: false,
+      		url: 'http://ecx.images-amazon.com/images/I/51XQoAJK1KL.jpg'
+    	},
+    	{
+      		itemName: 'Ice Bucket',
+      		borrowed: false,
+      		url: 'http://www.bizearch.com/images/products/11/05/110533.jpg'
+    	}
+    	
+    ],
+    password: '3g7k7k2ndk4'
+  }
+];
