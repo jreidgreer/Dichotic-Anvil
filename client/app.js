fa79748157733b36bfa8e92b1d15c25aa5e07fc1
Hack Reactor Students <accounts@hackreactor.com>
2016-04-08 20:11:52 -0700

    Has user, login, signup views and client side routers

--- client/app.js
+++ client/app.js
@@ -1,4 +1,29 @@
-var borrowApp = angular.module('borrowApp', []);
+var borrowApp = angular.module('borrowApp', ['ngRoute']);
 
-borrowApp.config('')
+borrowApp.config(function($routeProvider) {
+  $routeProvider
 
+  .when('/login', {
+    templateUrl : '/views/login.html',
+    controller: 'loginController'
+  })
+
+  .when('/signup', {
+    templateUrl : '/views/signup.html',
+    controller: 'signupController'
+  })
+
+  .when('/user', {
+    templateUrl : '/views/user.html',
+    controller: 'userController'
+  })
+});
+
+borrowApp.controller('loginController', function($scope){
+});
+
+borrowApp.controller('signupController', function($scope){
+});
+
+borrowApp.controller('userController', function($scope){
+});
