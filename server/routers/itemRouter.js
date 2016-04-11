760853030d6a24a524b2f1aee5277d450def0227
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-11 12:25:19 -0700

    Has item router and user router

---
+++ server/routers/itemRouter.js
@@ -0,0 +1,6 @@
+ var itemRouter = require('express').Router();
+ var itemController = require('../controllers/itemController.js');
+
+ 
+
+ module.exports = itemRouter;
---
+++ server/routers/userRouter.js
@@ -0,0 +1,6 @@
+ var userRouter = require('express').Router();
+ var userController = require('../controllers/userController.js');
+
+ 
+
+ module.exports = userRouter;
