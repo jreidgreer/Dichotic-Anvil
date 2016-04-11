52bb1c91d77b041c3ae217a19351986bcadf63eb
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-11 12:38:06 -0700

    Has routes that point to proper controllers for CRUD actions

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,6 +1,14 @@
  var userRouter = require('express').Router();
  var userController = require('../controllers/userController.js');
 
- 
+userRouter.route('/') // to all users (/api/users/)
+  .get(userController.retrieveAll)
+  .post(userController.createOne);
+
+
+userRouter.route('/:user_id') // to one specific user (/api/users/:user_id)
+  .get(userController.retrieveOne)
+  .put(userController.updateOne)
+  .delete(userController.deleteOne);
 
  module.exports = userRouter;
