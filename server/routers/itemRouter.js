061dc679f6901d2e1c785ddcbd94518cab4c370f
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-11 12:41:51 -0700

    Has routes that point to controller for CRUD actions

--- server/routers/itemRouter.js
+++ server/routers/itemRouter.js
@@ -1,6 +1,14 @@
- var itemRouter = require('express').Router();
- var itemController = require('../controllers/itemController.js');
+var itemRouter = require('express').Router();
+var itemController = require('../controllers/itemController.js');
 
- 
+itemRouter.route('/') // to all items (/api/items/)
+  .get(itemController.retrieveAll)
+  .post(itemController.createOne);
+
+
+itemRouter.route('/:item_id') // to one specific user (/api/items/:item_id)
+  .get(itemController.retrieveOne)
+  .put(itemController.updateOne)
+  .delete(itemController.deleteOne);
 
  module.exports = itemRouter;
