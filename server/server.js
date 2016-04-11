49a702ff7480491d3febcf1bf23516de5699c8de
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-11 12:24:29 -0700

    Has routes for users and items

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
+// BASE SETUP
+//============================================
+
 var express = require('express');
-var mongoose = require('mongoose');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/borrow');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static('client'));
+
+var port = process.env.PORT || 3000;
+
+// ROUTES FOR OUR API
+//============================================
+
+var userRouter = require('./routers/userRouter.js');
+var itemRouter = require('./routers/itemRouter.js');
+
+app.use('/api/users', userRouter);
+app.use('/api/items', itemRouter);
+
 
 app.get('/', function(req, res) {
   res.send('successful get request');
@@ -14,4 +34,9 @@ app.post('/', function(req, res) {
 
 
 
-app.listen(3000);
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Borrow App up and running on port:  ' + port);
+});
