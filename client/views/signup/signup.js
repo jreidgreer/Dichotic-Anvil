220c193e01d2be4f3f0df6587fbc091dddcfa72b
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-12 13:43:24 -0700

    Has controller function that sends post request for creating a user

--- client/views/signup/signup.js
+++ client/views/signup/signup.js
@@ -1,1 +1,22 @@
-//Signup controller
+angular.module('borrow.signup', [])
+.controller('signUpCtrl', function($scope, $http) {
+
+  $scope.postData = function() {
+    
+    var data = {
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      userName: $scope.userName,
+      password: $scope.password,
+      created_At: Date()
+    };
+
+    $http.post('/api/users/', data)
+      .then(function(resp) {
+        console.log(resp.data)
+      })
+      .catch(function(err) {
+        console.log('this', err);
+      })
+    }
+});
