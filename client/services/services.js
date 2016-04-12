88a97ed65c176aa848c89512770eb8b8851f78b7
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-12 13:42:35 -0700

    Has commented out code of where to begin with factory functions

---
+++ client/services/services.js
@@ -0,0 +1,13 @@
+// angular.module('borrow.services', [])
+//   .factory('Users', function($http) {
+//     var createUser = function(user, callback) {
+//       return http ({
+//         method: 'POST',
+//         url: '/api/users/',
+//         data: user
+//       })
+//       .then(function(resp) {
+//         callback(resp.data)
+//       });
+//     };
+//   });
