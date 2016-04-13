85162997be85055caa63b092a8b17bc88457ecf6
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-12 22:22:27 -0700

    Has login function for front end

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,20 +1,20 @@
 angular.module('borrow.login', [])
-.controller('loginController', function($scope, $http) {
+.controller('loginController', function($scope, $http, $location, User) {
 
-  $scope.postData = function() {
+  $scope.login = function() {
     
     var data = {
       userName: $scope.userName,
-      password: $scope.password,
-      created_At: Date()
+      password: $scope.password
     };
 
-    $http.post('/api/users/', data)
-      .then(function(resp) {
-        console.log(resp.data)
-      })
-      .catch(function(err) {
-        console.log('err: ', err);
-      })
+   User.login(data, function(resp) {
+    if (!resp.error) {
+      $location.path('/dashboard');
+    } else {
+      $scope.error = 'Incorrect username or password!!!!!';
     }
+   });
+  
+  };
 });
