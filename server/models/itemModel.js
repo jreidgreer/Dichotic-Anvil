d0b443fdcc87b4fcc0fc1de380de66bfa6abe630
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-04-13 15:32:44 -0700

    pre save item scema

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -5,9 +5,19 @@ var itemSchema = new Schema({
   _item : {type: Number, ref : 'User' },
   itemName : String,
   borrowed : Boolean,
-  imageUrl: String
-})
+  picture: {type: Schema.Types.Mixed},
+  morePictures: Schema.Types.Mixed, // this is not required
+  createdAt: {type: Date, default: Date.now}
+});
 
+itemSchema.pre('save', function(next){
+    now = new Date();
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
 //Create a model using the schema
 var Item = mongoose.model('Item', itemSchema);
 module.exports = Item;
+
