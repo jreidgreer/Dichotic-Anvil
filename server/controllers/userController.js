c002ba4f99234b2b7b9d855313ccfb12750934db
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-12 22:30:34 -0700

    Verify login function checks username for password match in db

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,33 +15,23 @@ exports.createOne = function(req, res) {
   });
 };
 
-exports.retrieveOne = function(req, res) {
-  var validate = {userName: req.body.userName};
-  User.findOne(validate, function(err, user) {
-    console.log('USER============',user);
+exports.verifyLogin = function(req, res) {
+  var username = {userName: req.body.userName};
+  User.findOne(username, function(err, user) {
     if(!user) {
-      res.send({error: 'invalid email or password'});
+      res.send({error: 'no user found'});
     } else {
       if (req.body.password === user.password) {
-        // res.redirect('/dashboard');
-        res.send({message: 'SUCCESSFUL'})
+        res.send({
+          username: user.userName,
+          inventory: user.inventory
+        });
       } else {
         res.send({error: 'invalid email or password'});
       }
     }
   });
 };
-
-
-  // //have to look in req.params to check for the id. Set the _id to to the value in req.params.id
-  // var query = {_id: req.params.user_id};
-  // User.findOne(query, function(err, matchingUser){
-  //   if(err){
-  //     return res.json(err);
-  //   } 
-  //   res.json(matchingUser);  
-  // });
-// };
 
 exports.retrieveAll = function(req, res) {
   var query = req.query;
