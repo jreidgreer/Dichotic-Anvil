9e54aff2d77fa38c5052c7785cff29d9d5bbda91
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-13 13:48:02 -0700

    Adds signout button

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,4 +1,8 @@
 //Dashboard controller
 
 angular.module('borrow.dashboard', [])
-  .controller('dashController', function($scope) {});
+  .controller('dashController', function($scope, Auth) {
+    $scope.signout = function() {
+      Auth.signout();
+    };
+});
