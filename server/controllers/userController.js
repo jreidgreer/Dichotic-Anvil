fcb09ad45af21cf47f47ca1fb496b8ba3078f07f
John Zuccarelli <zucca22@gmail.com>
2016-04-14 16:54:41 -0700

    auth fixed and user has inventory items.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,19 +7,49 @@ var jwt = require('jwt-simple'); // used to create, sign, and verify tokens
 // CRUD ACTIONS
 //============================================
 
+
+// only these fields get output'd to the client from the user model
+exports.userWhiteList = [
+  "userName",
+  "firstName",
+  "lastName",
+  "inventory"
+];
+
+// Use this to dish out an error to the client
+exports.sendError =  function(res, errorString){
+
+  error = {
+    success: false,
+  };
+  if(errorString)
+    error['error'] = errorString;
+
+  res.status(403).json(error);
+};
+
+// DOES NOT NEED MIDDLEWARE
 exports.createOne = function(req, res) {
   var newUser = req.body;
   User.create(newUser, function(err, user) {
     if(err) {
+
       console.log(err);
-      return res.json(err);
+
+      // user probably already exists, refuse request
+      exports.sendError(res, 'Username already exists');
+
+      return;
     }
-    console.log(user);
-    res.json(user);
+
+    // create new session
+    exports.createNewSessionForUser(user, res);
   });
 };
 
-exports.verifyLogin = function(req, res, next) {
+// DOES NOT NEED MIDDLEWARE
+exports.verifyLogin = function(req, res) {
+
   var userName = req.body.userName;
   var password = req.body.password;
 
@@ -28,39 +58,126 @@ exports.verifyLogin = function(req, res, next) {
 
   User.findOne({userName: userName}, function(err, user) {
     if(!user) {
-      next(console.error('User not found', err));
+      // no user with that username in database
+      exports.sendError(res, 'Invalid username or password');
+
     } else {
+
+      // compare the bcrypt passwords
       user.comparePasswords(password, function(err, compareResult){
+
         if (err) {
+          // unknown bcrypt compare error
           console.error('Error with password comparison', err);
         }
+
         if(compareResult) {
-          var token = jwt.encode(user, 'shhhhh');
-          res.json({token: token, user: {userName: user.userName, userId: user._id}});
+
+            // create new session
+            exports.createNewSessionForUser(user, res);
         } else {
-          return next(console.log('Passwords do not match'));
+
+          exports.sendError(res, 'Invalid username or password');
         }
+
       });
     }
   });
 };
 
-exports.authCheck = function (req, res) {
+
+// creates a new jwt-session for a newly created or newly logged in user
+exports.createNewSessionForUser = function (user, res) {
+
+  var session_vars = {
+      userName: user.userName,
+      created: Date.now(),
+  };
+
+  var session_secret = 'shhhhh';
+
+  // encrypt the session with ONLY our session vars so that we don't over-encrypt (all subdocuments and un-necessary fields)
+  var token = jwt.encode(session_vars, session_secret);
+          
+  console.log('Creating new session for user: [' + user.userName + '] --> token: [' + token + ']');
+
+  // dish out response that we made the session
+  res.status(201).json({
+    token: token,
+    success: true
+  });
+};
+
+// REQUIRES MIDDLEWARE
+exports.getUser = function(req, res) {
+
+  var userId = req.params.user_id;
+  if(userId === 'me') {
+    // map to the current userId
+    userId = req.currentUser._id;
+  }
+
+  User.findOne({_id: userId})
+  .populate('inventory')
+  .exec(function(err, foundUser) {
+        if (foundUser){
+
+          var response_object = {};
+
+          // process the whitelist
+          for(var k in foundUser) {
+            if(exports.userWhiteList.indexOf(k) > -1) {
+              response_object[k] = foundUser[k];
+            }
+          }
+            res.json(response_object);
+        } else {
+            exports.sendError(res, 'No user found with that ID');
+        }
+    })
+
+};
+
+
+// OUR "IS LOGGED IN?" MIDDLEWARE FUNCTION
+exports.authCheck = function (req, res, cb) {
   var token = req.headers['x-access-token'];
   if (!token) {
-    res.json({error: 'No token'});
+
+    cb(false);
+
   } else {
-    var user = jwt.decode(token, 'shhhhh');
+
+    var user = null;
+    try {
+      user = jwt.decode(token, 'shhhhh');
+    }
+    catch(err) {
+      user = null;
+    }
+
+    // bad token
+    if(!user) {
+      cb(false);
+      return;
+    }
+
     User.findOne({userName: user.userName}, function(err, foundUser) {
-        if (foundUser) {
-          res.send(200);
+        if (foundUser){
+
+            // we have the currently logged in user, set the currentUser variable on the request so that we can use it everywhere
+            req.currentUser = foundUser;
+            cb(true);
+
         } else {
-          res.send(401);
+
+            cb(false);
         }
     })
   }
 }
 
+// NEEDS MIDDLEWARE
 exports.retrieveAll = function(req, res) {
   var query = req.query;
   User.find(query, function(err, allUsers){
@@ -71,6 +188,7 @@ exports.retrieveAll = function(req, res) {
   });
 };
 
+// NEEDS MIDDLEWARE
 exports.updateOne = function(req, res) {
   var query = {_id: req.params.user_id};
   var updatedProps = req.body;
@@ -83,6 +201,7 @@ exports.updateOne = function(req, res) {
   });
 };
 
+// NEEDS MIDDLEWARE
 exports.deleteOne = function(req, res) {
   var query = {_id: req.params.user_id};
   User.findOneAndRemove(query, function(err, matchingUser){
