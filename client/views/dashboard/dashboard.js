b0051a3d5cc26aeb8e685f98024ba08584c1eb77
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-04-14 06:59:37 -0700

    add items to dashboard

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,8 +1,19 @@
 //Dashboard controller
 
 angular.module('borrow.dashboard', [])
-  .controller('dashController', function($scope, Auth) {
-    $scope.signout = function() {
-      Auth.signout();
-    };
+  .controller('dashController', function($scope, Auth, $http) {
+  $scope.items = [];
+    //Retrieve all the superheroes to show the gallery
+  $http.get('/api/items')
+  .success(function(data){
+    console.log(JSON.stringify(data));
+     $scope.items = data;
+  })
+  .error(function(data) {
+        console.log('Error: ' + data);
+  });
+
+  $scope.signout = function() {
+  Auth.signout();
+  };
 });
