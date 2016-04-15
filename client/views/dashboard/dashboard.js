b9788580a8ac93c83b22005303e751951c8b5c2e
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-15 14:17:10 -0700

    Display number of items on user page/dashboard

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -3,23 +3,26 @@
 angular.module('borrow.dashboard', [])
   .controller('dashController', function($scope, $http, Auth) {
 
-  $scope.items = [];
+    $scope.items = [];
 
-  $http.get('/api/user/me')
-  .success(function(data){
-    console.log(JSON.stringify(data));
-     $scope.user = data;
-     console.log($scope.user)
+    $http.get('/api/user/me')
+      .success(function(data){
+        console.log(JSON.stringify(data));
+         $scope.user = data;
+         console.log($scope.user)
+    
+        $scope.numPersonalItems = $scope.user.inventory.length
 
-     for (var i = 0; i < $scope.user.inventory.length; i++) {
-      $scope.items.push($scope.user.inventory[i].picture.url)
-     }
-  })
-  .error(function(data) {
-        console.log('Error: ' + data);
-  });
+        for (var i = 0; i < $scope.user.inventory.length; i++) {
+          $scope.items.push($scope.user.inventory[i].picture.url)
+        }
+      })
+      .error(function(data) {
+            console.log('Error: ' + data);
+      });
+  
+    $scope.signout = function() {
+      Auth.signout();
+    };
 
-  $scope.signout = function() {
-  Auth.signout();
-  };
 });
