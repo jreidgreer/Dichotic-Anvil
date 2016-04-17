a80b4a0a8c9d9fef832e939e4002b7b45061e14b
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-16 21:28:43 -0700

    Creates profile view and controller

---
+++ client/views/profile/profile.js
@@ -0,0 +1,42 @@
+//Dashboard controller
+
+angular.module('borrow.profile', [])
+  .controller('profileController', function($routeParams, $scope, $http, Auth) {
+
+  $scope.items = [];
+  $scope.myFriend = '';
+
+  var user_id = $routeParams.user_id;
+  // Get the user object to populate dashboard
+  $http.get('/api/user/' + user_id)
+    .success(function(data){
+      // console.log(JSON.stringify(data));
+      $scope.user = data;
+      console.log($scope.user)
+
+      $scope.numPersonalItems = $scope.user.inventory.length;
+      // Iterate over inventory to populate user items
+      for (var i = 0; i < $scope.user.inventory.length; i++) {
+        $scope.items.push($scope.user.inventory[i].picture.url)
+       }
+      // Iterate over inventory to populate items that have requests
+      for (var i = 0; i < $scope.user.inventory.length; i++) {
+        if($scope.user.inventory[i].requested) {
+          $scope.requestItems.push($scope.user.inventory[i]);
+        }
+      }
+      // Push strings to requestMessages array
+      for(var i = 0; i < $scope.requestItems.length; i++) {
+        $scope.requestMessages.push($scope.requestItems[i].whoWantsIt + ' would like to borrow your ' + $scope.requestItems[i].itemName)
+      }
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+  });   
+
+
+  $scope.signout = function() {
+    Auth.signout();
+  };
+
+});
