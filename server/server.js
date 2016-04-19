ded07c3f4e63d022353cdc82e5938ecab3ce3571
Arun Vasudevan <arunv83@gmail.com>
2016-04-19 16:48:43 -0700

    Update server.js to include Passport

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+// SESSION & PASSPORT
+//============================================
+var session = require('express-session');
+var passport = require('passport');
+
 mongoose.connect('mongodb://localhost/borrow');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,6 +21,11 @@ var port = process.env.PORT || 3000;
 // ROUTES FOR OUR API
 //============================================
 require('./routers/routes.js')(app, express);
+
+// ROUTES FOR FACEBOOK CONFIG
+//============================================
+require('./config/facebookConfig.js')(app, session, passport);
+require('./config/routes.js')(app, express, passport);
 
 // START THE SERVER
 //============================================
